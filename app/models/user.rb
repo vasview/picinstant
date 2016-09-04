72f00901b618365f73b7d95b211ce298d766a4df
Vasily Toropchin <vasview@gmail.com>
2016-09-05 01:26:55 +0600

    Level 1 was completed. Devise was setup and configured.
    Devise was setup for User model. In addition index on nickname field in the User model was added. Validations for fields (fullname, nickname) of the User model were configured.
    
    Pages controller was generated. Root path was setup to pages/index.
    
    Sign_in and sign_out functionality was checked.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+	has_many :photos
+
+	validates :fullname, presence: true, length: { maximum: 50}
+
+	validates :nickname, presence: true, 
+			length: { maximum: 50},
+			uniqueness: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  root 'pages#index'
+
+  get 'pages/show'
+
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
